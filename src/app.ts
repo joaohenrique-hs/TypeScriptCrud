92582acac9a7679131987a085439a994a59d652a
João Henrique <joaohenrique.hs@hotmail.com>
2020-05-04 19:24:52 -0300

    chore: Initial setup

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express from 'express'
+import cors from 'cors'
+import mongoose from 'mongoose'
+
+class App {
+  public express: express.Application
+
+  public constructor () {
+    this.express = express()
+
+    this.middlewares()
+    this.routes()
+  }
+
+  private middlewares (): void {
+    this.express.use(express.json())
+    this.express.use(cors())
+  }
+
+  private routes ():void {
+    this.express.get('/', (req, res) => {
+      return res.send('Hello world')
+    })
+  }
+}
+
+export default new App().express
+
