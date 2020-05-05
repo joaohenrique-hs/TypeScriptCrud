bb99fba1b7fcc236e259b4b882980e2721aee3a5
João Henrique <joaohenrique.hs@hotmail.com>
2020-05-05 00:08:06 -0300

    feat: Create route file and User route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import mongoose from 'mongoose'
+import routes from './routes'
 
 class App {
   public express: express.Application
@@ -23,6 +24,10 @@ class App {
       useNewUrlParser: true,
       useUnifiedTopology: true
     })
+  }
+
+  private routes ():void {
+    this.express.use(routes)
   }
 }
 
