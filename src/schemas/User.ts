792ede4737cf2090b8fcd5116181cb3501da7701
João Henrique <joaohenrique.hs@hotmail.com>
2020-05-05 20:19:50 -0300

    feat: Restructure user Model

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -1,15 +1,23 @@
 import { Schema, model, Document } from 'mongoose'
+import uuid from 'uuid/dist/v4'
 
 interface UserInterface extends Document {
+  id?: string,
+  name?: string
+  age?: number,
   email?: string,
-  firstName?: string
-  lastName?: string
+  password?: string
 }
 
 const UserSchema = new Schema({
+  _id: {
+    type: String,
+    default: uuid()
+  },
+  name: String,
+  age: Number,
   email: String,
-  firstName: String,
-  lastName: String
+  password: String
 }, {
   timestamps: true
 })
